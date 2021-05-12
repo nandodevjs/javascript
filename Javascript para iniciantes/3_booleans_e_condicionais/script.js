6e6051adc7ca6edf48f339267f92c17b1cef4ef1
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-12 00:28:35 -0300

    Update script.js

--- Javascript para iniciantes/3_booleans_e_condicionais/script.js
+++ Javascript para iniciantes/3_booleans_e_condicionais/script.js
@@ -14,3 +14,17 @@ if (nome) {
 } else {
     console.log("Me diga seu nome né o cabeção");
 }
+
+if ((5 - 5) && (5 + 5)) {
+    console.log("Verdadeiro");
+} else {
+    console.log("Falso");
+}
+
+var condicional = (5 - 10) && (5 + 5);
+
+if (condicional) {
+    console.log("Super Verdadeiro", condicional);
+} else {
+    console.log("Super falso");
+}
