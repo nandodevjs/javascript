ea12d5c0c5b7064ca5db4097aa3908774da4b60c
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-15 07:33:57 -0300

    condicionais_prontas

---
+++ Javascript para iniciantes/3_booleans_e_condicionais/exercicio.js
@@ -0,0 +1,35 @@
+var fernandoIdade = 21
+var tucoIdade = 10
+
+if (fernandoIdade < tucoIdade) {
+    console.log("Tuquinho é lindu <3");
+} else {
+    console.log("Tuquinho é mais lindo ainda <3");
+}
+
+var expressao = (5 - 2) && (5 - " ") && (5 - 2)
+console.log(expressao);
+
+var nome = "Tuco"
+var idade = 10
+var possuiDoutorado = true
+var empregoFuturo
+var dinheiroNaConta = 932818937182737634372643285475834754769856957
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !! dinheiroNaConta);
+
+
+var brasil = 207
+var china = 1340
+console.log(brasil === china);
+
+if (("Gato" === "gato") && (5 > 2)) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+if (("Gato" === "gato") || (5 > 2)) {
+    console.log("Gato" && "Cão");
+} else {
+    console.log("False");
+}
--- Javascript para iniciantes/3_booleans_e_condicionais/script.js
+++ Javascript para iniciantes/3_booleans_e_condicionais/script.js
@@ -28,3 +28,37 @@ if (condicional) {
 } else {
     console.log("Super falso");
 }
+
+true && true;// true
+true && false;// true
+false && true;// true
+"Gato" && "Cão";// Gato
+"Gato" && false;// Gato
+(5 - 5) && (5 + 5);// 10
+(5 >= 5) && (3 < 6);// true
+
+true || true;// true
+true || false;// true
+false || true;// true
+"Gato" || "Cão";// Gato
+"Gato" || false;// Gato
+(5 - 5) || (5 + 5);// 10
+(5 >= 5) || (3 < 6);// true
+
+// Switch
+
+var corFavorita = "Coro"
+
+switch (corFavorita) {
+    case "Introdução":
+        console.log("Na Eternidade")
+        break
+    case "Coro":
+        console.log("Ao lado de Jesus");
+        break
+    case "Segunda Parte":
+        console.log("Anceio logo estar");
+        break
+    default:
+        console.log("E este mundo deixar");
+}
