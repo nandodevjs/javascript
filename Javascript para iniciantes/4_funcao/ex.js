8ff240400e00beb07a04a3e6fe80334745b6dc10
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-18 22:54:27 -0300

    function_part_3

---
+++ Javascript para iniciantes/4_funcao/ex.js
@@ -0,0 +1,42 @@
+// Crie uma função para verificar se um valor é True
+
+function truDaTru (dado) {
+    return !!dado
+}
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// Lembrando que um perímetro é a soma dos quatro lados de um quadrado
+
+function perimetroDoQuadrado (lado) {
+   return lado * 4
+}
+
+// Crie uma função que retorne o seu nome completo
+// Ela deve possuir os parâmetros: nome e sobrenome
+
+function nomeCompleto4 (nome, sobreNome) {
+    return `${nome} ${sobreNome}`
+}
+
+// Crie uma função que verifica se um número é par
+
+function isEven (numero) {
+    var modulo = numero % 2
+    if (modulo === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Crie uma função que retorne o tipo de dado do argumento passado nela (typeof)
+
+ function tipoDeDado (dado) {
+     return typeof dado
+ }
+
+// addEventListener é uma função nativa do JavaScript, o primeiro parâmetro é o evento que ocorre e o segundo é o Callback. Utilize essa função para mostrar no console o seu nome completo todas as vezes quando o evento "click" ocorrer.
+
+addEventListener("click", function (){
+    console.log("Fernando");
+})
--- Javascript para iniciantes/4_funcao/script.js
+++ Javascript para iniciantes/4_funcao/script.js
@@ -50,9 +50,10 @@ console.log(pesquisa);
 
 // Chamadas de Callback, geralmente são funções que ocorrem após algum evento.
 
-addEventListener ("click", function () {
+/*addEventListener ("click", function () {
    console.log("Clicou");
-})
+})*/
+
 /*
 A função possui dois argumentos.
 Primeiro é a string "Click";
@@ -60,3 +61,67 @@ O segundo é uma função anônima.
 
 Funções anônimas são aquelas em que o nome da função não é definido, escritos como: function () {} ou () => {}
 */
+
+// Pode ou não retornar um valor
+
+// Quando não definimos o return, ela irá retonar undefined. O código interno da função  é executado normalmente, independente de existir valor de return ou não.
+
+function imc2 (peso, altura) {
+   const imc2 = peso / (altura * altura);
+   console.log(imc2);
+}
+
+imc2 (80, 1.80); // Retorna o imc
+console.log(imc2(80, 1.80)); // Retorna o imc e undefined
+
+// Valores retornados
+
+//Uma função pode retornar qualquer tipo de dado e até outras funções
+
+function terceiraIdade (idade) {
+   if (typeof idade !== "number"){
+      return "Informe a sua idade"
+   } else if (idade >= 60) {
+      return true
+   } else {
+      return false
+   }
+}
+
+console.log(terceiraIdade (60));
+// Cuidado, retornar diferentes tipos de dados na mesma função não é uma boa prática.
+
+// Escopo
+
+// Variáveis e funções definidas dentro de um bloco {}, não são visíveis fora dele.
+
+function precisoVisitar (paisesVisitados) {
+   var totalPaises = 193;
+   return `Falta apenas ${totalPaises - paisesVisitados}, paises`
+
+}
+console.log(precisoVisitar(10));
+
+// Escopo Léxico
+
+//Funções conseguem acessar variáveis que foram criadas no contexto pai.
+
+var profisao = "Desenvolvedor"
+
+function dados () {
+   var nome = "Fernando"
+   var idade = 18
+   function outrosDados () {
+      var endereco = "Gramado"
+      var idade = 10
+      return `${nome}, ${idade}, ${endereco}, ${profisao},`
+   }
+   return outrosDados()
+}
+
+console.log(dados()) // Retorna "Fernando", 10, Gramado, Desenvolvedor.
+//outrosDados () // Retorna um erro de não definido porque está dentro do bloco da função dados.
+
+//Hoisting
+
+// Antes de execultar uma função, o JS "move" todas as funções declaradas para a memória e depois vem execultando elas.
