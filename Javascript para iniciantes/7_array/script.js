cd62da3117357fba71fab7e05389fd90093b7ad6
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-24 18:58:58 -0300

    array

---
+++ Javascript para iniciantes/7_array/script.js
@@ -0,0 +1,11 @@
+// Array
+
+// É um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável.
+
+var videoGames = ["PS5", "Switch", "Xbox" ]
+
+videoGames [0]
+videoGames [1]
+videoGames [2]
+
+// Acesse um elemento do array utilizando []
