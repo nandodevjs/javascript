62bdaffc9eaeca40514fbc9b12cf3b55e146bb66
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-24 21:32:26 -0300

    array_e_loops

--- Javascript para iniciantes/7_array/script.js
+++ Javascript para iniciantes/7_array/script.js
@@ -2,10 +2,36 @@
 
 // É um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável.
 
-var videoGames = ["PS5", "Switch", "Xbox" ]
-
-videoGames [0]
-videoGames [1]
-videoGames [2]
-
+var videoGames = ["PS5", "Switch", "Xbox"]
+console.log(videoGames [0]);
+console.log(videoGames [1]);
+console.log(videoGames [2]);
+console.log(videoGames [3]);
 // Acesse um elemento do array utilizando []
+
+// Métodos e Propriedades de um Array
+
+console.log(videoGames.pop());
+videoGames.push("3DS")
+console.log(videoGames);
+console.log(videoGames.length);
+
+// Existem diversos outros métodos, como map, reduce, forEach e mais que veremos mais a frente
+
+// For loop
+
+// Fazem algo repetidamente até que uma condição seja atingida.
+
+for (var numero = 0; numero <= 10; numero++) {
+    console.log(numero);
+}
+// O for possui 3 partes que são elas: início, condição e incremento.
+
+// While loop
+
+var i = 0;
+
+while (i < 10) {
+    console.log(i);
+    i++
+}
