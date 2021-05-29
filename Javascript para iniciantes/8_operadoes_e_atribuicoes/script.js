81c8646fb47fa1adcb888300dccfdc0a1068ad32
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-28 23:05:21 -0300

    Operadores

---
+++ Javascript para iniciantes/8_operadoes_e_atribuicoes/script.js
@@ -0,0 +1,17 @@
+// Operadores de Atribuição
+
+// Podem funcionar como formas abreviadas
+
+var x = 5
+var y = 5
+
+x += y
+x -= y
+x *= y
+x /= y
+x %= y
+x **= y
+
+// X paassa a receber o útimo valor atribuido a ele que veio do Y
+
+console.log(x);
