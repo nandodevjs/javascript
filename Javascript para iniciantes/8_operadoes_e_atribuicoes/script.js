b6c57ea3d254d031a6474d13108423b77f9ad6f2
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-31 12:30:42 -0300

    finish

--- Javascript para iniciantes/8_operadoes_e_atribuicoes/script.js
+++ Javascript para iniciantes/8_operadoes_e_atribuicoes/script.js
@@ -15,3 +15,31 @@ x **= y
 // X paassa a receber o útimo valor atribuido a ele que veio do Y
 
 console.log(x);
+
+// Operador ternário
+
+// Abreviação de condicionais com if e else.
+
+var idade = 19
+var podeBeber = (idade >= 18) ? "Pode beber" : "Não pode beber"
+
+// Condição ? true : False
+
+// Geralmente utilizado quando precisamos atribuir um valor para uma variável, dependendo de uma condição.
+
+// If Abreviado
+
+// Não é necessário abrir e fechar as chaves {} quando retornamos uma linha de código.
+
+var possuiFaculdade = true
+if (possuiFaculdade) console.log("Possui faculdade");
+else console.log("Não possui faculdade.");
+
+//ou
+
+if (possuiFaculdade)
+    console.log("Possui faculdade");
+else
+    console.log("Não possui faculdade");
+
+// Eu particupalmente prefiro a segunda opção.
