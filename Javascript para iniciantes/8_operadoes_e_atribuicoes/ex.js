10220dc792e102935cc3c2e709858a164f483878
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-05-31 12:41:59 -0300

    Att

---
+++ Javascript para iniciantes/8_operadoes_e_atribuicoes/ex.js
@@ -0,0 +1,12 @@
+// Some 500 ao valor de scroll abaixo atribuindo o novo valor a scroll
+
+var scroll = 1000
+scroll += 500
+
+console.log(scroll);
+
+// Atribua true para a variaável darCredito, caso o cliente possua carro e casa, e false caso o contrário.
+
+var possuiCarro = false
+var possuiCasa = true
+var darCredito = (possuiCarro && possuiCasa)
