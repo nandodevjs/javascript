8fc56d32dc8a1c1310a4a3bd0be2569711c33b63
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-06-02 22:16:01 -0300

    escopo_fim

---
+++ Javascript para iniciantes/9_escopo/ex.js
@@ -0,0 +1,33 @@
+// Por qual motivo o código abaixo retorna com erros?
+
+{
+    var cor = "preto"
+    const marca = "Fiat"
+    let portas = 4
+    console.log(cor, marca, portas);
+}
+
+// Como corrigir o erro abaixo?
+const dois = 2
+
+function somarDois (x) {
+    return x + dois
+}
+
+function dividirDois (x) {
+    return x / dois
+}
+
+console.log(somarDois(4));
+console.log(dividirDois(2));
+
+// Como retornar o total de 500?
+
+const numero = 10
+
+for (let numero = 0; numero <= 50; numero++) {
+    console.log(numero);
+}
+
+const total = numero * 10
+console.log(total);
