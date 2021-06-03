1210a0f992f45aaa65b6c0b1a4f20ccdb46b7225
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-06-02 21:55:44 -0300

    Update script.js

--- Javascript para iniciantes/9_escopo/script.js
+++ Javascript para iniciantes/9_escopo/script.js
@@ -51,3 +51,48 @@ console.log(carro4);
 // Mesmo com a condição falsa, a variável ainda será declarada utilizando hoisting e o valor ficará como undefined.
 
 // A partir de agora ireomos utilizar const e let no lugar de var.
+
+// Criar um bloco
+
+// Chaves {} criam um escopo de bloco. Não confundir com a criação de objetos {}
+
+{
+    var carro5 = "Prisma"
+    const ano = 2018
+}
+
+console.log(carro5); // Prisma
+//console.log(ano); // Erro ano is not defined
+
+// Ao utilizar o var dentro de um for loop, que é um bloco, o valor da variável utilizada irá vazar e existir fora do loop.
+
+for (var i = 0; i < 10; i++) {
+    console.log(`Número: ${i}`);
+}
+console.log(i);
+
+// Const mantém o escopo no bloco, isso impede a redeclaração e impede a modificação do valor da variável, evitando bugs no código.
+
+const mes = "Janeiro";
+// mes = "Dezembro" // Erro! Tentou modificar o valor
+// const semana // Erro! Declarou sem valor
+
+const data = {
+    dia: 24,
+    mes: "Dezembro",
+    ano: 2021,
+}
+
+data.dia = 25 // Funciona
+// data = "Janeiro" // Erro!
+
+// Let
+
+// Mantém o escopo no bloco, impede a redeclaração, mas permite a modificação do valor da variável.
+
+let ano
+ano = 2021
+ano++
+console.log(ano);
+
+// let ano = 2023 //Erro! "Ano" já foi declarado.
