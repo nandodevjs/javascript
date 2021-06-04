c63dce47e3a7084baf478acf02817c0c5ad9dc35
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-06-04 11:27:52 -0300

    dom_update

--- 2_Dom_Para_Iniciantes/1_O_que_e_o_DOM/script.js
+++ 2_Dom_Para_Iniciantes/1_O_que_e_o_DOM/script.js
@@ -10,10 +10,24 @@
 
 //window.alert("Olá")
 
-const href = window.location.href
-console.log(href);
+// const href = window.location.href
+// console.log(href);
 
-if (href === "http://127.0.0.1:5500/2_Dom_Para_Iniciantes/1_O_que_e_o_DOM/"){
-    console.log("SEXTOOOOOU");
-}
+// if (href === "http://127.0.0.1:5500/2_Dom_Para_Iniciantes/1_O_que_e_o_DOM/"){
+//    console.log("SEXTOOOOOU");
+// }
+
+// DOM: Window > Location : Document : ... > HTML > <head> : <body> > <h1> : <section> > <h2> > <p>
+
+// Window e Document
+
+// São os objetos principais do DOM. Boa parte da manipulação é feita através dos seus métodos e propriedades.
+
+// window.alert("Isso é um alerta")
+// alert("Isso é um alerta") // Funciona
+
+const h1Selecionado = document.querySelector("h1") // Seleciona o primeiro h1
+document.body // Retorna o body
+
+// Window é um objeto global,por isso não precisamos chamar ele na frente dos seus métodos e propriedades.
 
