f14a5e47b80683ef3f28f4134f115a05560c0a25
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-06-04 10:39:01 -0300

    Dom

---
+++ 2_Dom_Para_Iniciantes/1_O_que_e_o_DOM/script.js
@@ -0,0 +1,19 @@
+// Document Object Model
+
+// É uma interface que representa documentos HTML e XML através de objetos. Com ela é possível manipular a estrutura, estilo e conteúdo destes documentos.
+
+// console.log(window); // Windown é o objeto global do browser, possui diferentes métodos e propriedades.
+
+// window.innerHeight // Retorna a altura do beowser
+
+// Ao inspecionar elemento com o Chrome, você está vendo a representação oficial da DOM.
+
+//window.alert("Olá")
+
+const href = window.location.href
+console.log(href);
+
+if (href === "http://127.0.0.1:5500/2_Dom_Para_Iniciantes/1_O_que_e_o_DOM/"){
+    console.log("SEXTOOOOOU");
+}
+
